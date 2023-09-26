316697c74c5311cf150d85594bd0fc683d0bd3ca
David Castro Gómez <david.castro@enti.cat>
2023-09-26 09:45:13 +0200

    Enums

--- app/src/main/java/com/example/apuntesclasecdi/MainActivity.kt
+++ app/src/main/java/com/example/apuntesclasecdi/MainActivity.kt
@@ -13,8 +13,35 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.apuntesclasecdi.ui.theme.ApuntesClaseCDITheme
+
+enum class Colors(val color: Color, val tranlationKey: String){
+    Red(Color.Red, "Color_Key_Red"),
+    Green(Color.Green,"Color_Key_Green"),
+    Blue(Color.Blue,"Color_Key_Blue"),
+
+    /*fun GetColor() : Color {
+        when(this){
+            Red -> Color.Red
+            Green -> Color.Green
+            Blue -> Color.Blue
+        }
+
+        return Color.Black;
+    }*/
+}
+
+enum class Operations(val operation: (Int, Int) -> Int, val priority: Int){
+    Add({a,b -> a + b}, 0),
+    Subtract({a,b -> a - b}, 0);
+
+    fun Calculate(a: Int, b: Int): Int {
+        return this.operation(a,b)
+    }
+}
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,33 +51,15 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Column(){
+                        var color = Colors.Green
+                        color.color
 
-                        for(i in 1 .. 5){
-                            PrintScreen(null)
-                        }
+                        PrintScreen(color.tranlationKey, color = color.color)
 
-                        Spacer(modifier = Modifier.height(16.dp))
+                        val operation = Operations.Add
+                        val result = operation.Calculate(1, 2)
 
-                        val names = listOf("Pedro", "David");
-
-                        for(name in names) {
-                            PrintScreen(name)
-                        }
-
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        for(i in names.indices){
-                            //PrintScreen(names.indexOf(i))
-                        }
-
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        names.forEach{name -> PrintScreen(name)}
-
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        //names.forEachIndexed{1 name -> PrintScreen()}
-
+                        PrintScreen(name = result.toString())
                     }
 
 
@@ -61,21 +70,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun PrintScreen(name: String?, modifier: Modifier = Modifier) {
-
-    val name = name ?: run{
-        Log.e("NULO", "ERES MALO")
-        return
-    }
-
-    /*val name2 = name2 ?: run{
-        Log.e("NULO2", "Y ADEMAS HUELES MAL")
-        return
-    }*/
+fun PrintScreen(name: String, modifier: Modifier = Modifier, color: Color? = null) {
 
     Text(
-        text = "Hello ${name?.uppercase()}",
-        modifier = modifier
+        text = "Hello ${name.uppercase()}",
+        modifier = modifier,
+        color = color ?: Color.Black
     )
 
 }
