c6d93b98d882e88c6aad1d2f907adf6ad265d696
David Castro Gómez <david.castro@enti.cat>
2023-10-10 11:02:35 +0200

    Monke neither does know how to make commits

--- app/src/main/java/com/example/apuntesclasecdi/ButtonGrid1.kt
+++ app/src/main/java/com/example/apuntesclasecdi/ButtonGrid1.kt
@@ -1,4 +1,66 @@
-package com.example.apuntesclasecdi
+package com.example.apuntesclasecdi;
 
-class ButtonGrid1 {
+
+import android.content.Context
+import android.view.ViewParent
+import android.widget.Button
+import android.widget.LinearLayout;
+
+public class ButtonGrid(val context: Context, val parent: LinearLayout) {
+
+    class ButtonsRow(val context: Context, val parent: LinearLayout){
+        var buttons : MutableList<Button> = mutableListOf()
+        val  linearLayout: LinearLayout = LinearLayout(context)
+
+        init {
+            linearLayout.orientation = LinearLayout.HORIZONTAL
+            linearLayout.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                1.0f
+            )
+
+            linearLayout.setBackgroundColor(context.getColor(R.color.teal_200))
+
+            parent.addView(linearLayout)
+        }
+
+        fun AddNewButton(text: String) : Button{
+            var bt = Button(context)
+            bt.text = text
+
+            bt.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            1.0f
+            )
+
+            linearLayout.addView(bt)
+            buttons.add(bt)
+
+            return bt
+        }
+    }
+
+
+
+    private val rows: MutableList<ButtonsRow> = mutableListOf();
+
+    fun AddNewRow() : ButtonsRow {
+
+        val btRow = ButtonsRow(context, parent)
+
+        rows.add(btRow)
+
+        return btRow
+
+    }
+
+    fun GetRow(index: Int) : ButtonsRow?{
+        if(index >= rows.count() || index < 0){
+            return null
+        }
+
+        return rows[index]
+    }
 }
--- app/src/main/java/com/example/apuntesclasecdi/MainActivity.kt
+++ app/src/main/java/com/example/apuntesclasecdi/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.apuntesclasecdi
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,16 +20,100 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.core.widget.doBeforeTextChanged
-import com.example.apuntesclase.R
 import com.example.apuntesclasecdi.ui.theme.ApuntesClaseCDITheme
 
 class MainActivity : ComponentActivity() {
 
-    var counter = 0
+    class ButtonGrid;
+
+    enum class Operation(private val operation: (a: Int, b: Int) -> Int, char: Char){
+        Add({a,b -> a + b}, " + "),
+        Sub({a,b -> a - b}," - ")),
+        Mul({a,b -> a * b," * ")),
+        Div({a,b -> a / b}," / "));
+
+        public fun Operate(a: Int, b: Int) : Int{
+            return this.operation(a,b)
+        }
+    }
+
+    var A:Int? = null
+    var B:Int? = null
+    var Op:Operation? = null
+
+    val result : TextView by lazy {findViewById(R.id.number)}
+    val btncontainer : LinearLayout by lazy {findViewById(R.id.btncontainer)}
+
+    fun OperationPress(op: Operation){
+        if(A != null && B != null){
+            Op = op
+            result.text = A.toString() + op?.toString()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_screen)
+
+        val buttonGrid = ButtonGrid(this, btncontainer)
+
+        var names: MutableList<MutableList<String>> = mutableListOf()
+        names.add(mutableListOf("AC", "()", "%", "/"))
+        names.add(mutableListOf("7", "8", "9", "x"))
+        names.add(mutableListOf("4", "5", "6", "-"))
+        names.add(mutableListOf("1", "2", "3", "+"))
+        names.add(mutableListOf("0", ",", "<-", "="))
+
+        for(y in names.indices){
+            var row = buttonGrid.AddNewRow()
+
+            for(x in names[y].indices){
+                val bt = row.AddNewButton(names[y][x])
+
+                bt.setOnClickListener{
+                    when(names[y][x]){
+                        "AC" ->{
+                            A = null
+                            B = null
+                            Op = null
+                            result.text ="Sad monke"
+                        }
+                        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ->{
+                            if(Op == null){
+                                A?.let { a ->
+                                    A = (a * 10) + names[y][x].toInt()
+                                }?: run{
+                                    A = names[y][x].toInt()
+                                }
+                            }else{
+                                B?.let { b ->
+                                    B = (b * 10) + names[y][x].toInt()
+                                }?: run{
+                                    B = names[y][x].toInt()
+                                }
+                            }
+                        }
+                        "+"->{
+                            OperationPress(Operation.Add)
+                        }
+                        "-"->{
+                            OperationPress(Operation.Sub)
+                        }
+                        "*"->{
+                            OperationPress(Operation.Mul)
+                        }
+                        "/"->{
+                            OperationPress(Operation.Div)
+                        }
+                        "="->{
+
+                        }
+                    }
+                }
+            }
+        }
+
+
 
     }
 }
