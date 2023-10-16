834d9483c7c24175b1931a32b705fff14856e8a9
David Castro Gómez <david.castro@enti.cat>
2023-10-16 10:59:09 +0200

    monkey is tired

--- app/src/main/java/com/example/apuntesclasecdi/MainActivity2.kt
+++ app/src/main/java/com/example/apuntesclasecdi/MainActivity2.kt
@@ -1,7 +1,6 @@
 package com.example.apuntesclasecdi
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -26,19 +25,10 @@ import com.example.apuntesclasecdi.ui.theme.ApuntesClaseCDITheme
 import com.example.apuntesclasecdi.utilBannan.SharedBannan
 import com.example.apuntesclasecdi.utilBannan.banan
 
-class MainActivity : ComponentActivity() {
-
-    val changeScreenBt by lazy{ findViewById<Button>(R.id.change_screen_bt)}
+class MainActivity2 : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.start_screen)
-
-        changeScreenBt.setOnClickListener{
-            val newIntent = Intent(this, MainActivity2::class.java)
-
-            //se pone el this porque somos tontos y nos han dado por culo
-            this.startActivity(newIntent)
-        }
 
 }}
